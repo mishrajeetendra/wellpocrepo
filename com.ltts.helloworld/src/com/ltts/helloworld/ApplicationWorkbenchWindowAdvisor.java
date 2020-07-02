84d3915a23f52bc21fe48c931c3cf6dbaf3de4bf
Jeetendra Mishra <you@example.com>
2020-07-02 15:01:44 +0530

    initial commit

---
+++ com.ltts.helloworld/src/com/ltts/helloworld/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,25 @@
+package com.ltts.helloworld;
+
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
+
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	@Override
+	public void preWindowOpen() {
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setShowCoolBar(false);
+		configurer.setShowStatusLine(false);
+	}
+}
